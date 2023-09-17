547ae18dc26624c89fd1f7bd28119e68b6da0996
mr-aun <kiattisakjanjam@gmail.com>
2023-09-17 15:53:44 +0700

    Add project files.

---
+++ PokemonReviewApp/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Review
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string text { get; set; }
+        public Reviewer Reviewer { get; set; }
+        public Pokemon Pokemonter { get; set; }
+    }
+}
---
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Reviewer
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
